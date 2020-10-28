521835d9d155abfd940fc55db854166e3f9a53d9
summerDora <15988484252@163.com>
2020-10-28 10:37:41 +0800

    ref

--- src/App-ref.vue
+++ src/App-ref.vue
@@ -9,6 +9,12 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import {ref} from 'vue'
+/*
+-   ref和reactive一样，也是用来实现响应式数据的方法
+-   ref的本质还是reactive，系统会根据我们传给ref的值将他转化成reactive
+        +   ref(18)->reactive({value:18})
+        +   所以更改ref数据时，需要 变量名.value=新值
+*/
 export default {
     name: 'App',
     components: {
